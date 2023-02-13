3db238d534ce9daa8ba5382d7804499b966cc36b
LandoADeveloper <landom2222@gmail.com>
2023-02-13 14:36:23 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+const toTranslateField = document.getElementById("toTranslateField")
+const translateButton = document.querySelector("input[type='button']")
+const outputField = document.querySelector("section.outputField")
+
+
+  
+  
+
+translateButton.addEventListener("click", () => {
+    let toTranslateText = toTranslateField.value
+    toTranslateText = toTranslateText.toUpperCase()
+    console.log("test")
+    let morseArray = [...toTranslateText].map((letter, index) => {
+        let lexica = checkLexica(letter)
+        console.log(lexica);
+        return lexica.morseCode
+    })
+    renderOutput(morseArray)
+
+
+})
+
+function checkLexica(letter) {
+    console.log(letter)
+    return morseAlphabet.find(obj => {
+        return obj.letter === letter
+    })
+}
+
+
+
+function renderOutput(output) {
+    outputField.innerHTML = `
+        <p>${output.join(" ")}</p>
+    ` 
+}
+let morseAlphabet = [
+    { letter: "1", morseCode: ".----" },
+    { letter: "2", morseCode: "..---" },
+    { letter: "3", morseCode: "...--" },
+    { letter: "4", morseCode: "....-" },
+    { letter: "5", morseCode: "....." },
+    { letter: "6", morseCode: "-...." },
+    { letter: "7", morseCode: "--..." },
+    { letter: "8", morseCode: "---.." },
+    { letter: "9", morseCode: "----." },
+    { letter: "0", morseCode: "-----" },
+    { letter: " ", morseCode: "    " },
+    { letter: "A", morseCode: ".-" },
+    { letter: "B", morseCode: "-..." },
+    { letter: "C", morseCode: "-.-." },
+    { letter: "D", morseCode: "-.." },
+    { letter: "E", morseCode: "." },
+    { letter: "F", morseCode: "..-." },
+    { letter: "G", morseCode: "--." },
+    { letter: "H", morseCode: "...." },
+    { letter: "I", morseCode: ".." },
+    { letter: "J", morseCode: ".---" },
+    { letter: "K", morseCode: "-.-" },
+    { letter: "L", morseCode: ".-.." },
+    { letter: "M", morseCode: "--" },
+    { letter: "N", morseCode: "-." },
+    { letter: "O", morseCode: "---" },
+    { letter: "P", morseCode: ".--." },
+    { letter: "Q", morseCode: "--.-" },
+    { letter: "R", morseCode: ".-." },
+    { letter: "S", morseCode: "..." },
+    { letter: "T", morseCode: "-" },
+    { letter: "U", morseCode: "..-" },
+    { letter: "V", morseCode: "...-" },
+    { letter: "W", morseCode: ".--" },
+    { letter: "X", morseCode: "-..-" },
+    { letter: "Y", morseCode: "-.--" },
+    { letter: "Z", morseCode: "--.." }
+  ];
